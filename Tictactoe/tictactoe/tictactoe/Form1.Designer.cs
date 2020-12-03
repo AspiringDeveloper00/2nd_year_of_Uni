b6dc015763b75e3d72d583f46fd3c3cfb3bd0153
Antonis Karnavas <grand1925@yahoo.gr>
2020-12-03 02:20:42 +0200

    Add files via upload

---
+++ Tictactoe/tictactoe/tictactoe/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace tictactoe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(238, 186);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(373, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Play!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "PC",
+            "Human"});
+            this.comboBox1.Location = new System.Drawing.Point(470, 186);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.resetToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ Tictactoe/tictactoe/tictactoe/Form1.cs
@@ -0,0 +1,445 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tictactoe
+{
+    public partial class Form1 : Form
+    {
+        private string player1 = "x";
+        private string player2 = "o";
+        int scoreplayer1 = 0;
+        int scoreplayer2 = 0;
+        int n;
+        Random rnd = new Random();
+        int randombutton;
+        string gameplay;
+        Button[] buttons;
+        bool turn = true;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            Int32.TryParse(textBox1.Text, out n);
+            if(!(n <= 3 || n % 2 == 0) & !(comboBox1.SelectedItem == null))
+            {
+                if (comboBox1.SelectedItem.ToString() == "PC")
+                {
+                    gameplay = "p";
+                }
+                else if (comboBox1.SelectedItem.ToString() == "Human")
+                {
+                    gameplay = "h";
+                }
+                textBox1.Visible = false;
+                button1.Visible = false;
+                comboBox1.Visible = false;
+                n = n * n;
+                SetButtons();
+                SetGrid();
+            }
+            else
+            {
+                MessageBox.Show("Try Again! Pick an odd number equal to 5 or higher! Pick your opponent! ", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox1.Text = "";
+            }
+            
+        }
+
+
+ //Reset table when there is a winner
+        public void winnersreset()
+        {
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].Enabled = true;
+                buttons[i].BackColor = Color.White;
+                buttons[i].Text = "";
+                a = 0;
+            }
+        }
+        int a = 0;
+
+        public void gamereset()
+        {
+            winnersreset();
+            Application.Restart();
+        }
+        //Check winner
+        public bool winners()
+        {
+            bool winner = false;
+            int count=0;
+            int y = Convert.ToInt32(Math.Sqrt(n));
+            //Columns x
+            for (int j=0;j<= y-1;j++)
+            {
+                for (int i = j; i <n; i = i + y)
+                {
+                    if (i == j) { count = 0; }
+
+                    if (buttons[i].Text == player1)
+                    {
+                        count = count + 1;
+                    }
+                }
+                if (count == y)
+                {
+                    for (int i = j; i < n; i = i + y)
+                    {
+                        buttons[i].BackColor = Color.Blue;
+                    }
+                    MessageBox.Show("X is Winner");
+                    scoreplayer1 += 1;
+                    l1.Text = "Score of player X: " + scoreplayer1.ToString();
+                    winnersreset();
+                    winner = true;
+                }
+            }
+
+            //Rows x
+            for (int j = 0; j <n; j = j + y)
+            {
+                for (int i = j; i <=j+(y-1); i++)
+                {
+                    if (i == j) { count = 0; }
+
+                    if (buttons[i].Text == player1)
+                    {
+                        count = count + 1;
+                    }
+                }
+                if (count == y)
+                {
+                    for (int i = j; i <= j + (y - 1); i++)
+                    {
+                        buttons[i].BackColor = Color.Blue;
+                    }
+                    MessageBox.Show("X is Winner");
+                    scoreplayer1 += 1;
+                    l1.Text = "Score of player X: " + scoreplayer1.ToString();
+                    winnersreset();
+                    winner = true;
+                }
+            }
+
+            //Main diagonal x
+            for (int i = 0; i < n; i = i + (y + 1))
+            {
+                if(i == 0) { count = 0; }
+
+                if (buttons[i].Text == player1)
+                {
+                    count = count + 1;
+                }
+            }
+
+            if (count == y)
+            {
+                for (int i = 0; i < n; i = i + (y + 1))
+                {
+                    buttons[i].BackColor = Color.Blue;
+                }
+                MessageBox.Show("X is Winner");
+                scoreplayer1 += 1;
+                l1.Text = "Score of player X: " + scoreplayer1.ToString();
+                winnersreset();
+                winner = true;
+            }
+
+            //Secondary diagonal x
+            for(int i = y - 1; i <= n - y; i = i + (y - 1))
+            {
+                if (i == y-1) { count = 0; }
+
+                if (buttons[i].Text == player1)
+                {
+                    count = count + 1;
+                }
+            }
+
+            if (count == y)
+            {
+                for (int i = y - 1; i <= n - y; i = i + (y - 1))
+                {
+                    buttons[i].BackColor = Color.Blue;
+                }
+                MessageBox.Show("X is Winner");
+                scoreplayer1 += 1;
+                l1.Text = "Score of player X: " + scoreplayer1.ToString();
+                winnersreset();
+                winner = true;
+            }
+
+
+            //Columns O
+            for (int j = 0; j <= y - 1; j++)
+            {
+                for (int i = j; i < n; i = i + y)
+                {
+                    if (i == j) { count = 0; }
+
+                    if (buttons[i].Text == player2)
+                    {
+                        count = count + 1;
+                    }
+                }
+                if (count == y)
+                {
+                    for (int i = j; i < n; i = i + y)
+                    {
+                        buttons[i].BackColor = Color.Red;
+                    }
+                    MessageBox.Show("O is Winner");
+                    scoreplayer2 += 1;
+                    l2.Text = "Score of player O: " + scoreplayer2.ToString();
+                    winnersreset();
+                    winner = true;
+                }
+            }
+
+            //Rows O
+            for (int j = 0; j < n; j = j + y)
+            {
+                for (int i = j; i <= j + (y - 1); i++)
+                {
+                    if (i == j) { count = 0; }
+
+                    if (buttons[i].Text == player2)
+                    {
+                        count = count + 1;
+                    }
+                }
+                if (count == y)
+                {
+                    for (int i = j; i <= j + (y - 1); i++)
+                    {
+                        buttons[i].BackColor = Color.Red;
+                    }
+                    MessageBox.Show("O is Winner");
+                    scoreplayer2 += 1;
+                    l2.Text = "Score of player O: " + scoreplayer2.ToString();
+                    winnersreset();
+                    winner = true;
+                }
+            }
+
+            //Main diagonal O
+            for (int i = 0; i < n; i = i + (y + 1))
+            {
+                if (i == 0) { count = 0; }
+
+                if (buttons[i].Text == player2)
+                {
+                    count = count + 1;
+                }
+            }
+
+            if (count == y)
+            {
+                for (int i = 0; i < n; i = i + (y + 1))
+                {
+                    buttons[i].BackColor = Color.Red;
+                }
+                MessageBox.Show("O is Winner");
+                scoreplayer1 += 1;
+                l2.Text = "Score of player O: " + scoreplayer2.ToString();
+                winnersreset();
+                winner = true;
+            }
+
+            //Secondary diagonal O
+            for (int i = y - 1; i <= n - y; i = i + (y - 1))
+            {
+                if (i == y - 1) { count = 0; }
+
+                if (buttons[i].Text == player2)
+                {
+                    count = count + 1;
+                }
+            }
+
+            if (count == y)
+            {
+                for (int i = y - 1; i <= n - y; i = i + (y - 1))
+                {
+                    buttons[i].BackColor = Color.Red;
+                }
+                MessageBox.Show("O is Winner");
+                scoreplayer1 += 1;
+                l2.Text = "Score of player O: " + scoreplayer2.ToString();
+                winnersreset();
+                winner = true;
+            }
+
+            a += 1;
+
+            if (gameplay == "h")
+            {
+                if (a == n & winner == false)
+                {
+                    MessageBox.Show("It's a tie");
+                    winnersreset();
+                }
+            }
+            else
+            {
+                if (a == n+1 & winner == false)
+                {
+                    MessageBox.Show("It's a tie");
+                    winnersreset();
+                }
+            }
+
+            if (scoreplayer1 == 5)
+            {
+                MessageBox.Show("PLAYER X IS THE FINAL WINNER", "Winner announcement", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                gamereset();
+            }else if (scoreplayer2 == 5)
+            {
+                MessageBox.Show("PLAYER O IS THE FINAL WINNER", "Winner announcement", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                gamereset();
+            }
+            return winner;
+        }
+        
+
+       
+        public void Game(object sender, EventArgs e)
+        {
+            if (gameplay == "h")
+            {
+                Button clicked = (Button)sender;
+                if (turn)
+                {
+                    clicked.Text = player1;
+                }
+                else
+                {
+                    clicked.Text = player2;
+                }
+
+                turn = !turn;
+                clicked.Enabled = false;
+
+                winners();
+            }
+            else
+            {
+
+                Button clicked = (Button)sender;
+                clicked.Text = player1;
+                clicked.Enabled = false;
+                winners();
+                while (true)
+                {
+                    randombutton = rnd.Next(n);
+                    if (buttons[randombutton].Enabled == true)
+                    {
+                        buttons[randombutton].Text = player2;
+                        buttons[randombutton].Enabled = false;
+                        winners();
+                        break;
+                    }
+                }
+                             
+
+            }
+        }
+
+
+
+
+
+
+        Label l1;
+        Label l2;
+        //Paint buttons in a 2d array
+        public void SetGrid()
+        {
+            int y = Convert.ToInt32(Math.Sqrt(n));
+            this.Size = new System.Drawing.Size(y * 100, y * 100);
+            l1 = new Label();
+            l2 = new Label();
+            l1.Font = new Font("Arial", 15, FontStyle.Bold);
+            l2.Font = new Font("Arial", 15, FontStyle.Bold);
+            l1.Size = new Size (400, 30);
+            l2.Size = new Size(400, 30);
+            l1.Location = new Point(30, (y * 100 - 100) / 2 + 100);
+            l2.Location= new Point(30, (y * 100 - 100) / 2 + 130);
+            l1.Text = "Score of player X: " + scoreplayer1.ToString();
+            l2.Text = "Score of player O: " + scoreplayer2.ToString();
+            l1.Parent = this;
+            l2.Parent = this;
+            Controls.Add(l1);
+            Controls.Add(l2);
+            int k = 0;
+            for (int j=30; j <= (y*100-100)/2 +30 ; j=j+50)
+            {
+                for (int i=0;i<= (y * 100 - 100); i = i + 100)
+                {
+                    buttons[k].Parent = this;
+                    buttons[k].Location = new Point(i, j);
+                    k = k + 1;
+                }
+                
+            }
+           
+
+            for (int i = 0; i < buttons.Length; i++)
+            {
+
+                buttons[i].Click += new EventHandler(Game);
+                buttons[i].BackColor = Color.White;
+
+
+                
+            }
+        }
+
+        //Initiate button array
+        public void SetButtons()
+        {
+            buttons = new Button[n];
+            for (int i = 0; i <= n - 1; i++)
+            {
+                buttons[i] = new Button();
+            }
+
+           
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Classic tic tac toe game that you can play vs the pc or a friend! First player to reach 5 wins, simply wins!");
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (button1.Visible == false)
+            {
+                gamereset();
+            }
+            else { Application.Restart(); }
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
